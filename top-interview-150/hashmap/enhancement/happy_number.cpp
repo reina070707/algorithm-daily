232577fa65ee1d67a3adffef9d13361b43d870f5
Reina Hirata <reina.taniguchi.hirata@gmail.com>
2026-01-06 13:06:17 +0900

    Enhancement: Happy Number

---
+++ top-interview-150/hashmap/enhancement/happy_number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unordered_set>
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        std::unordered_set<int> seen;
+
+        while (n >= 10 && seen.find(n) == seen.end()) {
+            seen.insert(n);
+
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            n = sum;
+        }
+
+        return n == 1 || n == 7;
+    }
+};

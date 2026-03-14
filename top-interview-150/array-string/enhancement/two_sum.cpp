f18a1f3ec1c012f12da0813ecd36111f282c7bf5
Reina Hirata <reina.taniguchi.hirata@gmail.com>
2026-03-14 11:59:42 +0900

    Two Sum: enhancement

--- top-interview-150/array-string/enhancement/two_sum.cpp
+++ top-interview-150/array-string/enhancement/two_sum.cpp
@@ -1,5 +1,7 @@
 #include <vector>
 #include <unordered_map>
+#include <ranges>
+#include <span>
 
 using namespace std;
 
@@ -8,12 +10,14 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // key and value
         unordered_map<int, int> seen;
+        namespace rv = std::ranges::views;
 
-        for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];
+        for (int i : rv::iota(0, static_cast<int>(nums.size()))) {
+            const int current_val = nums[i];
+            const int complement = target - current_val;
 
-            if (seen.find(complement) != seen.end()) {
-                return {seen[complement], i};
+            if (auto it = seen.find(complement); it != seen.end()) {
+                return {it->second, i};
             }
 
             seen[nums[i]] = i;
